c24c6b4fc92424250d556c6c946967a6dcf1958e
Brian Sakurada <sakuradabs@gmail.com>
2019-08-20 18:15:50 +0900

    The app works all the way through to the end. It could still use a graceful way of exiting.

--- app/src/main/java/com/iwateeikaiwa/intensifiers/MainActivity.java
+++ app/src/main/java/com/iwateeikaiwa/intensifiers/MainActivity.java
@@ -4,60 +4,85 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
 
     protected Button wordButton;
-    protected Button imageButton;
     protected TextView textView;
+    protected ImageView imageView;
+
+    protected int[] imageIds = {
+            R.drawable.angry,
+            R.drawable.baseball,
+            R.drawable.beach,
+            R.drawable.disneyland,
+            R.drawable.dog,
+            R.drawable.happy,
+            R.drawable.hotdog,
+            R.drawable.mansion,
+            R.drawable.mb,
+            R.drawable.pizza,
+            R.drawable.nintendo,
+            R.drawable.rollercoaster,
+            R.drawable.salad,
+            R.drawable.skier
+    };
+    int currentImgIndex;
+    int currentWordIndex;
+    String[] intensifiers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.imageView);
         wordButton = findViewById(R.id.getWordButton);
-        imageButton = findViewById(R.id.getImageButton);
         textView = findViewById(R.id.intensifierView);
+        Resources res = getResources();
+        intensifiers = res.getStringArray(R.array.intensifiers);
+
+        initializeGame();
 
         wordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText(getIntensifier());
+
+                haveAllWordsShown();
+                haveAllImagesShown();
+                if (currentImgIndex < imageIds.length) {
+                    imageView.setImageResource(imageIds[currentImgIndex]);
+                    textView.setText(intensifiers[currentWordIndex]);
+                    currentWordIndex++;
+                }
             }
         });
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO: add image switching.
-            }
-        });
-
     }
 
-    public String generateString(int length) {
-        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
-        StringBuilder stringBuilder = new StringBuilder();
-        Random random = new Random();
+    private void initializeGame() {
+        imageView.setImageResource(R.drawable.cover);
+        textView.setText(R.string.start);
+        currentWordIndex = 0;
+        currentImgIndex = 0;
+    }
 
-        for (int i = 0; i < length; i++) {
-            char c = chars[random.nextInt(chars.length)];
-            stringBuilder.append(c);
+    private void haveAllImagesShown() {
+        if (currentImgIndex == imageIds.length) {
+            // Finished
+            textView.setText(R.string.finished);
+
         }
-        return stringBuilder.toString();
     }
 
-
-    public String getIntensifier() {
-        Random random = new Random();
-        Resources res = getResources();
-        String[] intensifiers = res.getStringArray(R.array.intensifiers);
-        return intensifiers[random.nextInt(intensifiers.length)];
+    private void haveAllWordsShown() {
+        if (currentWordIndex == intensifiers.length) {
+            currentWordIndex = 0;
+            currentImgIndex++;
+        }
     }
+
 }
